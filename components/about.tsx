54c875da2595cf0f153f070daf2c5194ae5a0e75
jeevan-korah <jeevan@netx.co.in>
2025-08-28 20:25:38 +0530

    typo

--- components/about.tsx
+++ components/about.tsx
@@ -5,7 +5,7 @@ export default function About() {
         <div className="border-t py-12 [border-image:linear-gradient(to_right,transparent,--theme(--color-slate-400/.25),transparent)1] md:py-20">
           {/* Section header */}
           <div className="mx-auto max-w-3xl  text-center ">
-            {/* <div className="inline-flex items-center gap-3 pb-3 before:h-px before:w-8 before:bg-linear-to-r before:from-transparent before:to-indigo-200/50 after:h-px after:w-8 after:bg-linear-to-l after:from-transparent after:to-indigo-200/50">
+            {/* <div className="inline-flex items-center gap-4 pb-3 before:h-px before:w-8 before:bg-linear-to-r before:from-transparent before:to-indigo-200/50 after:h-px after:w-8 after:bg-linear-to-l after:from-transparent after:to-indigo-200/50">
               <span className="inline-flex bg-linear-to-r from-green-500 to-green-200 bg-clip-text text-transparent">
                 {sectionData.badge}
               </span>
